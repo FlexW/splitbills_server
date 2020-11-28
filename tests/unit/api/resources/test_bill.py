d38822b66701f7a1cddb54ebb8bb3b010e809c58
Felix Weilbach <felix.weilbach@t-online.de>
2020-11-28 15:40:44 +0100

    Add tests for change bill date

--- tests/unit/api/resources/test_bill.py
+++ tests/unit/api/resources/test_bill.py
@@ -7,6 +7,7 @@ from app.models.user import User, insert_user
 from app.models.bill import (Bill, insert_bill,
                              get_bill_by_id)
 from app.models.bill_member import BillMember
+from app.util.json_data_encoder import json_data_encoder
 
 
 def test_add_members_to_bill_if_bill_already_created(test_client, api_headers_auth):
@@ -177,6 +178,84 @@ def test_change_description_of_existing_bill(test_client, api_headers_auth):
 
     assert json_response["message"] == "Changed bill."
     assert bill.description == data["description"]
+
+
+def test_change_date_of_existing_bill(test_client, api_headers_auth):
+    password = "securepassword"
+    now = datetime.datetime.utcnow()
+
+    user1 = User(first_name="Max",
+                 last_name="Muster",
+                 email="muster@mail.de",
+                 password=password)
+    insert_user(user1)
+    user2 = User(first_name="Max",
+                 last_name="Muster",
+                 email="muster2@mail.de",
+                 password=password)
+    insert_user(user2)
+
+    bill_member1 = BillMember(user_id=user1.id, amount="5.00")
+    bill_member2 = BillMember(user_id=user2.id, amount="-5.00")
+
+    bill1 = Bill(description="Bill",
+                 date=now,
+                 date_created=now,
+                 members=[bill_member1, bill_member2])
+    bill_id = insert_bill(bill1).id
+
+    data = {
+        "date": datetime.datetime.utcnow()
+    }
+
+    response = test_client.put("/bills/{}".format(bill_id),
+                               headers=api_headers_auth(user1.email, password),
+                               data=json.dumps(data, default=json_data_encoder))
+    json_response = json.loads(response.get_data(as_text=True))
+
+    bill = get_bill_by_id(bill_id)
+
+    assert json_response["message"] == "Changed bill."
+    assert bill.date == data["date"]
+
+
+def test_dont_change_date_created_of_existing_bill(test_client, api_headers_auth):
+    password = "securepassword"
+    now = datetime.datetime.utcnow()
+
+    user1 = User(first_name="Max",
+                 last_name="Muster",
+                 email="muster@mail.de",
+                 password=password)
+    insert_user(user1)
+    user2 = User(first_name="Max",
+                 last_name="Muster",
+                 email="muster2@mail.de",
+                 password=password)
+    insert_user(user2)
+
+    bill_member1 = BillMember(user_id=user1.id, amount="5.00")
+    bill_member2 = BillMember(user_id=user2.id, amount="-5.00")
+
+    bill1 = Bill(description="Bill",
+                 date=now,
+                 date_created=now,
+                 members=[bill_member1, bill_member2])
+    bill_id = insert_bill(bill1).id
+
+    data = {
+        "date_created": datetime.datetime.utcnow()
+    }
+
+    response = test_client.put("/bills/{}".format(bill_id),
+                               headers=api_headers_auth(user1.email, password),
+                               data=json.dumps(data, default=json_data_encoder))
+    json_response = json.loads(response.get_data(as_text=True))
+
+    bill = get_bill_by_id(bill_id)
+
+    assert json_response["message"] == "Changed bill."
+    assert bill.date_created == now
 
 
 def test_dont_change_bill_if_amounts_sum_is_not_zero(test_client, api_headers_auth):
