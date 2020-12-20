2f75b99ea1256f5a37f5bfdaf56b9cd3a323679f
Felix Weilbach <felix.weilbach@t-online.de>
2020-12-20 15:38:12 +0100

    Add missing users resource tests

--- tests/unit/api/resources/test_users.py
+++ tests/unit/api/resources/test_users.py
@@ -1,8 +1,7 @@
 import pytest
 import json
 
-from app.models.user import User, get_user_by_email, get_all_users, insert_user
-from app.api.schemas.user import user_schema
+from app.models.user import get_user_by_email, get_all_users
 
 
 def test_add_non_existing_user(app, test_client, api_headers):
@@ -76,7 +75,25 @@ def test_error_on_first_name_missing(app, test_client, api_headers):
     assert len(get_all_users()) == 0
 
 
-def test_dont_add_user_without_last_name(app, test_client, api_headers):
+def test_error_on_first_name_wrong_type(app, test_client, api_headers):
+    user_data = {
+        "first_name": True,
+        "last_name": "Muster",
+        "email": "muster@mail.de",
+        "password": "securepassword"
+    }
+
+    response = test_client.post("/users",
+                                headers=api_headers,
+                                data=json.dumps(user_data))
+    json_respone = json.loads(response.get_data(as_text=True))
+
+    assert response.status_code == 400
+    assert json_respone["message"] == "Attribute first_name needs to be of type str"
+    assert len(get_all_users()) == 0
+
+
+def test_error_on_last_name_missing(app, test_client, api_headers):
     user_data = {
         "first_name": "Max",
         "email": "muster@mail.de",
@@ -93,7 +110,25 @@ def test_dont_add_user_without_last_name(app, test_client, api_headers):
     assert len(get_all_users()) == 0
 
 
-def test_dont_add_user_without_email(app, test_client, api_headers):
+def test_error_on_last_name_wrong_type(app, test_client, api_headers):
+    user_data = {
+        "first_name": "Max",
+        "last_name": True,
+        "email": "muster@mail.de",
+        "password": "securepassword"
+    }
+
+    response = test_client.post("/users",
+                                headers=api_headers,
+                                data=json.dumps(user_data))
+    json_respone = json.loads(response.get_data(as_text=True))
+
+    assert response.status_code == 400
+    assert json_respone["message"] == "Attribute last_name needs to be of type str"
+    assert len(get_all_users()) == 0
+
+
+def test_error_on_email_missing(app, test_client, api_headers):
     user_data = {
         "first_name": "Max",
         "last_name": "Muster",
@@ -110,7 +145,25 @@ def test_dont_add_user_without_email(app, test_client, api_headers):
     assert len(get_all_users()) == 0
 
 
-def test_dont_add_user_without_password(app, test_client, api_headers):
+def test_error_on_email_wrong_type(app, test_client, api_headers):
+    user_data = {
+        "first_name": "Max",
+        "last_name": "Muster",
+        "email": True,
+        "password": "securepassword"
+    }
+
+    response = test_client.post("/users",
+                                headers=api_headers,
+                                data=json.dumps(user_data))
+    json_respone = json.loads(response.get_data(as_text=True))
+
+    assert response.status_code == 400
+    assert json_respone["message"] == "Attribute email needs to be of type str"
+    assert len(get_all_users()) == 0
+
+
+def test_error_on_password_missing(app, test_client, api_headers):
     user_data = {
         "first_name": "Max",
         "last_name": "Muster",
@@ -124,4 +177,22 @@ def test_dont_add_user_without_password(app, test_client, api_headers):
 
     assert response.status_code == 400
     assert json_respone["message"] == "Missing attribute password"
+    assert len(get_all_users()) == 0
+
+
+def test_error_on_password_wrong_type(app, test_client, api_headers):
+    user_data = {
+        "first_name": "Max",
+        "last_name": "Muster",
+        "email": "muster@mail.de",
+        "password": True
+    }
+
+    response = test_client.post("/users",
+                                headers=api_headers,
+                                data=json.dumps(user_data))
+    json_respone = json.loads(response.get_data(as_text=True))
+
+    assert response.status_code == 400
+    assert json_respone["message"] == "Attribute password needs to be of type str"
     assert len(get_all_users()) == 0
