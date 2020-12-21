aaf50316c186d79082e7370a61d3ebd520f9f06c
Felix Weilbach <felix.weilbach@t-online.de>
2020-12-21 11:15:32 +0100

    Update groups bills resource to new api scheme

--- app/api/resources/group_bills.py
+++ app/api/resources/group_bills.py
@@ -1,16 +1,16 @@
-from flask import g
 from flask_restful import Resource
-from app import auth
+from flask_jwt_extended import jwt_required
 from app.api.schemas.bill import bills_schema
 from app.models.bill import get_valid_bills_by_group_id
+from app.api.resources.common import get_authorized_user
 
 
 class GroupBillsResource(Resource):
 
-    @auth.login_required
+    @jwt_required
     def get(self, group_id):
-        current_user = g.current_user
+        current_user = get_authorized_user()
 
         groups = get_valid_bills_by_group_id(current_user.id)
 
-        return {"bills": bills_schema.dump(groups)}
+        return {"bills": bills_schema.dump(groups)}, 200
--- tests/unit/api/resources/test_group_bills.py
+++ tests/unit/api/resources/test_group_bills.py
@@ -4,14 +4,11 @@ import datetime
 from app.models.group_member import GroupMember
 from app.models.group import Group, insert_group
 from app.models.user import User, insert_user
-from app.models.bill import (Bill,
-                             insert_bill,
-                             get_bills_by_user_id,
-                             get_all_bills)
+from app.models.bill import Bill, insert_bill
 from app.models.bill_member import BillMember
 
 
-def test_get_bills_from_group(test_client, api_headers_auth):
+def test_get_bills_from_group(test_client, api_headers_bearer, insert_tokens):
     password = "securepassword"
     now = datetime.datetime.utcnow()
 
@@ -20,6 +17,7 @@ def test_get_bills_from_group(test_client, api_headers_auth):
                  email="muster@mail.de",
                  password=password)
     insert_user(user1)
+    user1_tokens = insert_tokens(user1.email)
 
     user2 = User(first_name="Max",
                  last_name="Muster",
@@ -28,7 +26,8 @@ def test_get_bills_from_group(test_client, api_headers_auth):
     insert_user(user2)
 
     group = Group(name="Name",
-                  group_members=[GroupMember(user=user1), GroupMember(user=user2)])
+                  group_members=[GroupMember(user=user1),
+                                 GroupMember(user=user2)])
     insert_group(group)
 
     bill = Bill(description="Bill",
@@ -40,14 +39,16 @@ def test_get_bills_from_group(test_client, api_headers_auth):
     insert_bill(bill)
 
     response = test_client.get("/groups/{}/bills".format(group.id),
-                               headers=api_headers_auth(user1.email, password))
+                               headers=api_headers_bearer(
+                                   user1_tokens["access_token"]["token"]))
     json_response = json.loads(response.get_data(as_text=True))
 
+    assert response.status_code == 200
     assert len(json_response["bills"]) == 1
     assert json_response["bills"][0]["id"] == bill.id
 
 
-def test_get_only_bills_from_group(test_client, api_headers_auth):
+def test_get_only_bills_from_group(test_client, api_headers_bearer, insert_tokens):
     password = "securepassword"
     now = datetime.datetime.utcnow()
 
@@ -56,6 +57,7 @@ def test_get_only_bills_from_group(test_client, api_headers_auth):
                  email="muster@mail.de",
                  password=password)
     insert_user(user1)
+    user1_tokens = insert_tokens(user1.email)
 
     user2 = User(first_name="Max",
                  last_name="Muster",
@@ -64,7 +66,8 @@ def test_get_only_bills_from_group(test_client, api_headers_auth):
     insert_user(user2)
 
     group = Group(name="Name",
-                  group_members=[GroupMember(user=user1), GroupMember(user=user2)])
+                  group_members=[GroupMember(user=user1),
+                                 GroupMember(user=user2)])
     insert_group(group)
 
     bill1 = Bill(description="Bill",
@@ -82,8 +85,17 @@ def test_get_only_bills_from_group(test_client, api_headers_auth):
     insert_bill(bill2)
 
     response = test_client.get("/groups/{}/bills".format(group.id),
-                               headers=api_headers_auth(user1.email, password))
+                               headers=api_headers_bearer(
+                                   user1_tokens["access_token"]["token"]))
     json_response = json.loads(response.get_data(as_text=True))
 
     assert len(json_response["bills"]) == 1
     assert json_response["bills"][0]["id"] == bill1.id
+
+
+def test_needs_to_be_authenticated_on_get(test_client, api_headers):
+    response = test_client.get("/groups/1/bills", headers=api_headers)
+    json_response = json.loads(response.get_data(as_text=True))
+
+    assert response.status_code == 401
+    assert json_response["message"] == "Missing Authorization Header"
