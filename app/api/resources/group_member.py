7c14c1eaf4941d9e059c1c013394931424e4c4f5
Felix Weilbach <felix.weilbach@t-online.de>
2020-12-21 11:24:42 +0100

    Update group member resource to new api scheme

--- app/api/resources/group_member.py
+++ app/api/resources/group_member.py
@@ -1,10 +1,10 @@
-from flask import abort, request, g
+from flask import abort, request
+from flask_jwt_extended import jwt_required
 from flask_restful import Resource
-from app import auth
-from .common import (load_request_data_as_json,
-                     check_user_is_member_of_group,
-                     check_user_exists,
-                     check_group_exists)
+from app.api.resources.common import (check_user_is_member_of_group,
+                                      check_user_exists,
+                                      check_group_exists,
+                                      get_authorized_user)
 
 
 def _delete_user_from_group(user, group):
@@ -15,15 +15,15 @@ def _delete_user_from_group(user, group):
 
 class GroupMemberResource(Resource):
 
-    @auth.login_required
+    @jwt_required
     def delete(self, group_id, user_id):
         group = check_group_exists(group_id)
 
-        check_user_is_member_of_group(g.current_user, group)
+        check_user_is_member_of_group(get_authorized_user(), group)
 
         user = check_user_exists(user_id)
         check_user_is_member_of_group(user, group)
 
         _delete_user_from_group(user, group)
 
-        return {"message": "Deleted user from group."}
+        return {"message": "Deleted user from group"}, 200
--- tests/unit/api/resources/test_group_member.py
+++ tests/unit/api/resources/test_group_member.py
@@ -1,73 +1,82 @@
 import json
 
-from app.models.user import User, insert_user, get_all_users
-from app.models.group import Group, insert_group, get_group_by_id, get_all_groups
+from app.models.user import User, insert_user
+from app.models.group import Group, insert_group
 from app.models.group_member import GroupMember
 
 
-def test_delete_user_from_existing_group(test_client, api_headers_auth):
+def test_delete_user_from_existing_group(test_client, api_headers_bearer, insert_tokens):
     password = "securepassword"
 
     user1 = User(first_name="Max",
-                last_name="Muster",
-                email="muster@mail.de",
-                password=password)
+                 last_name="Muster",
+                 email="muster@mail.de",
+                 password=password)
     user1 = insert_user(user1)
+    user1_tokens = insert_tokens(user1.email)
 
     group_member1 = GroupMember(user=user1)
     group = Group(name="Muster",
                   group_members=[group_member1])
     insert_group(group)
 
-    response = test_client.delete("/groups/{}/members/{}".format(group.id, user1.id),
-                                  headers=api_headers_auth(user1.email, password))
+    response = test_client.delete("/groups/{}/members/{}"
+                                  .format(group.id, user1.id),
+                                  headers=api_headers_bearer(
+                                      user1_tokens["access_token"]["token"]))
     json_response = json.loads(response.get_data(as_text=True))
 
-    assert json_response["message"] == "Deleted user from group."
+    assert response.status_code == 200
+    assert json_response["message"] == "Deleted user from group"
     assert group_member1.valid == 0
 
 
-def test_dont_delete_user_if_user_not_in_group(test_client, api_headers_auth):
+def test_dont_delete_user_if_user_not_in_group(test_client, api_headers_bearer, insert_tokens):
     password = "securepassword"
 
     user1 = User(first_name="Max",
-                last_name="Muster",
-                email="muster@mail.de",
-                password=password)
+                 last_name="Muster",
+                 email="muster@mail.de",
+                 password=password)
     user1 = insert_user(user1)
 
     user2 = User(first_name="Max",
-                last_name="Muster",
-                email="muster2@mail.de",
-                password=password)
+                 last_name="Muster",
+                 email="muster2@mail.de",
+                 password=password)
     user2 = insert_user(user2)
+    user2_tokens = insert_tokens(user2.email)
 
     group_member1 = GroupMember(user=user1)
     group = Group(name="Muster",
                   group_members=[group_member1])
     insert_group(group)
 
-    response = test_client.delete("/groups/{}/members/{}".format(group.id, user1.id),
-                                  headers=api_headers_auth(user2.email, password))
+    response = test_client.delete("/groups/{}/members/{}"
+                                  .format(group.id, user1.id),
+                                  headers=api_headers_bearer(
+                                      user2_tokens["access_token"]["token"]))
     json_response = json.loads(response.get_data(as_text=True))
 
-    assert json_response["message"] == "Forbidden"
+    assert response.status_code == 401
+    assert json_response["message"] == "Group does not exist"
     assert group_member1.valid == 1
 
 
-def test_dont_delete_user_if_not_in_group(test_client, api_headers_auth):
+def test_dont_delete_user_if_not_in_group(test_client, api_headers_bearer, insert_tokens):
     password = "securepassword"
 
     user1 = User(first_name="Max",
-                last_name="Muster",
-                email="muster@mail.de",
-                password=password)
+                 last_name="Muster",
+                 email="muster@mail.de",
+                 password=password)
     user1 = insert_user(user1)
+    user1_tokens = insert_tokens(user1.email)
 
     user2 = User(first_name="Max",
-                last_name="Muster",
-                email="muster2@mail.de",
-                password=password)
+                 last_name="Muster",
+                 email="muster2@mail.de",
+                 password=password)
     user2 = insert_user(user2)
 
     group_member1 = GroupMember(user=user1)
@@ -75,10 +84,21 @@ def test_dont_delete_user_if_not_in_group(test_client, api_headers_auth):
                   group_members=[group_member1])
     insert_group(group)
 
-    response = test_client.delete("/groups/{}/members/{}".format(group.id, user2.id),
-                                  headers=api_headers_auth(user1.email, password))
+    response = test_client.delete("/groups/{}/members/{}"
+                                  .format(group.id, user2.id),
+                                  headers=api_headers_bearer(
+                                      user1_tokens["access_token"]["token"]))
     json_response = json.loads(response.get_data(as_text=True))
 
-    assert json_response["message"] == "Forbidden"
+    assert response.status_code == 401
+    assert json_response["message"] == "Group does not exist"
     assert group_member1.valid == 1
     assert len(group.group_members) == 1
+
+
+def test_needs_to_be_authenticated_on_delete(test_client, api_headers):
+    response = test_client.delete("/groups/1/members/1", headers=api_headers)
+    json_response = json.loads(response.get_data(as_text=True))
+
+    assert response.status_code == 401
+    assert json_response["message"] == "Missing Authorization Header"
